f91475857bbefdbe724acad1e1b3897d9af4a0cb
Julius Schlosburg <icecreamguy@gmail.com>
2012-05-01 18:30:51 -0700

    Added text to README, cleaned up some

--- demo.php
+++ demo.php
@@ -1,4 +1,22 @@
 <?php
+/*
+SQLMarkup by Julius Schlosburg May 2012
+
+This file is part of SQLMarkup.
+
+    SQLMarkup is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SQLMarkup is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SQLMarkup.  If not, see <http://www.gnu.org/licenses/>.
+*/
 include ('sqlMarkup.php');
 $demoStatement = <<<'EOT'
 SELECT toppingStyle, COUNT(*)  FROM foods INNER JOIN toppings ON foods foods.toppingStyle = foods.toppingStyle     WHERE food foods.type LIKE 'pizza' AND toppings.topping_id = 5
